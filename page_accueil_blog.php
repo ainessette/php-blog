4b561094cc61c8691fd9a13b4a66efdb1bc46e3f
sylvain <sylvain.guerrin347@orange.fr>
2018-02-18 23:10:39 +0100

    Ajout lien css dans la page php

--- page_accueil_blog.php
+++ page_accueil_blog.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <title>Mon blog</title>
+        <link rel="stylesheet" href="styles_blog.css" />
     </head>
     <body>
         <div id="contenu_page">
