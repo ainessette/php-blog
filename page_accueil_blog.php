85912ab56254be4cfd70ebc69b8a214e90a0238b
sylvain <sylvain.guerrin347@orange.fr>
2018-02-18 20:34:09 +0100

    Ajout de la page d'accueil du blog en html

---
+++ page_accueil_blog.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon blog</title>
+    </head>
+    <body>
+        <div id="contenu_page">
+            
+        </div>
+    </body>
+    
+<html/>
